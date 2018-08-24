d8decdc0d2251bd61974208ddbfe6d69a8efb881
jialiang <jialiangnull@gmail.com>
2018-08-24 13:18:59 +0800

    PJSIP 2.7.2 Build

--- build/pjproject/src/pjlib/include/pj/config.h
+++ build/pjproject/src/pjlib/include/pj/config.h
@@ -1,4 +1,4 @@
-/* $Id: config.h 5683 2017-11-08 03:03:22Z ming $ */
+/* $Id: config.h 5744 2018-02-21 02:58:39Z ming $ */
 /* 
  * Copyright (C) 2008-2011 Teluu Inc. (http://www.teluu.com)
  * Copyright (C) 2003-2008 Benny Prijono <benny@prijono.org>
@@ -1293,7 +1293,7 @@ PJ_BEGIN_DECL
 #define PJ_VERSION_NUM_MINOR	7
 
 /** PJLIB version revision number. */
-#define PJ_VERSION_NUM_REV	1
+#define PJ_VERSION_NUM_REV	2
 
 /**
  * Extra suffix for the version (e.g. "-trunk"), or empty for
